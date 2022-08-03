2c107ec30661335ca234a6f3e7af0c3e9211d396
ryo12211956 <96876520+ryo12211956@users.noreply.github.com>
2022-08-03 23:15:10 +0900

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>PHPの基本</title>
+    </head>
+    <body>
+        <?php
+        // print命令は指定された文字列を表示するための命令です。 
+        print 'こんにちは、世界！<br />';
+        print 'こんにちは、皆さん!';
+        ?>
+    </body>
+</html>
