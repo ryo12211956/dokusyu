eab96a0bbbd2741df79837db9ae375654f2f5593
ryo12211956 <96876520+ryo12211956@users.noreply.github.com>
2022-09-03 22:52:49 +0900

    Add files via upload

---
+++ const.php
@@ -0,0 +1,5 @@
+<?php
+  const TAX = 1.1;
+  $price = 1000;
+  $sum = $price * TAX;
+  print $sum; // 結果:1000
---
+++ var_variable.php
@@ -0,0 +1,4 @@
+<?php
+  $x = 'title';
+  $title = 'PHP:Hypertext Preprocessor';
+  print $$x; // 結果：PHP:Hypertext Preprocessor
